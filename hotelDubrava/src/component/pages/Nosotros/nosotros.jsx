f337fb1e882a8f4be896565148ae2e8cbe2caec9
Angel Borrego <angel_borr@hotmail.com>
2024-03-28 11:06:36 -0300

    incorporacion page Nosotros + responsive

--- hotelDubrava/src/component/pages/Nosotros/nosotros.jsx
+++ hotelDubrava/src/component/pages/Nosotros/nosotros.jsx
@@ -1,6 +1,72 @@
 const Nosotros = () => {
     return (
-        <h1>Hola desde Nosotros</h1>
+        <main className="pt-3 pb-3 ">
+            <div className="container border border-[#dd5657] pt-2 rounded font-sans text-justify">
+                <div>
+                    <p className="text-[#3f1010] w-full mb-0 pb-4 text-sm md:text-base lg:text-lg lg:leading-loose">
+                        En las tranquilas aguas de Mar de Ostende, allá por 1975, un grupo de amigos decidieron 
+                        levantar un hotel al que llamaron “Dubrava”, o “bosque de robles” en croata, honrando así 
+                        a su ciudad natal: “Dubrovnik”.
+                    </p>
+                </div>
+                <section className="md:flex md:gap-3 md:items-center">
+                    <div className="w-full pb-4 h-[250px] box-content md:basis-2/4 lg:basis-1/4 lg:h-[185px]">
+                        <img className=" rounded inline w-screen h-[250px] lg:h-[185px]" src="/img/imgNosotros-1.jpg" alt="FotoHistorica-1" />
+                    </div>
+                    <div className="md:basis-2/4 lg:basis-3/4">
+                        <p className="text-[#3f1010] w-full mb-0 pb-4 text-sm md:text-base lg:text-lg lg:leading-loose">
+                            Esta ciudad costera del Mar Adriático, situada en la actual Croacia, combina las 
+                            bellezas de sus muros medievales con las empinadas calles que se pierden subiendo
+                            y bajando de la montaña San Sergio. Toda esta belleza le ha dado el mote de
+                            "la perla del Adriático" y la ha enaltecido como la Atenas Eslava.
+                            Seguramente añorando aquel paisaje, fue que nació el Hotel Dubrava en una zona 
+                            agreste y poco habitada a las afueras de una Pinamar que recién daba sus primeros 
+                            pasos como ciudad veraniega.
+                        </p>
+                    </div>
+                </section>
+                <section className="md:flex md:gap-3 md:items-center">
+                    <div className="w-full pb-4 h-[250px] box-content md:basis-2/4 md:order-2 lg:basis-2/5 lg:h-[300px]">
+                        <img className=" rounded inline w-screen h-[250px] lg:h-[290px]" src="/img/imgNosotros-2.jpg" alt="FotoHistorica-2" />
+                    </div>
+                    <div className="md:basis-2/4 lg:basis-3/5">
+                        <p className="text-[#3f1010] w-full mb-0 pb-4 text-sm md:text-base lg:text-lg lg:leading-loose">
+                            En sus comienzos, el pequeño hotel contaba simplemente con un desayunador en la planta
+                            baja y una docena de habitaciones; lo distinguían su amplio parque, su cercanía de la playa
+                            y sus hermosas vistas, las habitaciones del este disfrutando del horizonte en las tranquilas
+                            aguas del atlántico mientras que las habitaciones del oeste refrescadas por el frondoso
+                            bosque pinamarense.
+                            Fue así que un buen día, allá por el 1978, la familia compuesta por Fermín González, 
+                            un intrépido emprendedor español oriundo de Santander, y Elizabeta Dominikovic, 
+                            quién casualmente nació y vivió hasta su adolescencia en Dubrovnik, compraron este pequeño 
+                            hotel escapándole al trajín de una Buenos Aires que empezaba a super-poblarse.
+                        </p>
+                    </div>
+                </section>
+                <section className="md:flex md:gap-3 md:items-center">
+                    <div className="w-full pb-4 h-[250px] box-content md:basis-2/4 lg:basis-1/4 lg:h-[210px]">
+                        <img className=" rounded inline w-screen h-[250px] lg:h-[210px]" src="/img/imgNosotros-3.jpg" alt="FotoHistorica-3" />
+                    </div>
+                    <div className="md:basis-2/4 lg:basis-3/4">
+                        <p className="text-[#3f1010] w-full mb-0 pb-4 text-sm md:text-base lg:text-lg lg:leading-loose">
+                            Con esfuerzo y tenacidad ambos lograron ampliar el hotel a lo que es hoy: un sencillo e 
+                            imponente hotel familiar con 2 pisos más de habitaciones y un ampliado desayunador. Sus 
+                            huéspedes buscan en el hotel un lugar para el descanso, con una ubicación ideal para 
+                            bajar a la playa, con fácil acceso ya sea al centro de Pinamar como a la majestuosa 
+                            Cariló o a la emprendedora Ostende.
+                            Como dijo alguna vez uno de nuestros huéspedes:“Aquí uno siente en carne propia la frase
+                            hecha: la hospitalidad de ser atendidos por sus propios dueños; uno se siente como en su 
+                            casa…”.
+                        </p>
+                    </div>
+                </section>
+                <div>
+                    <p className="text-[#3f1010] w-full mb-0 pb-4 text-sm md:text-base lg:text-lg lg:leading-loose">Todo esto hace del Hotel Dubrava un excelente lugar donde disfrutar de unas tranquilas
+                        vacaciones a las orillas del mar.
+                    </p>
+                </div>                
+            </div>
+        </main>
     )
 
 }
